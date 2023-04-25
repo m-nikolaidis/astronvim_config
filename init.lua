d51fd0b7d0e04480edd83e062ceca4e5e209c40e
marios <marionik23@gmail.com>
2023-04-25 11:24:18 +0300

    Enabled community plugin

--- init.lua
+++ init.lua
@@ -64,6 +64,20 @@ return {
     },
   },
 
+  -- configure my plugins
+  plugins = {
+    init = {
+      {
+        "folke/todo-comments.nvim",
+        requires = "nvim-lua/plenary.nvim",
+        config = function()
+          require("todo-comments").setup {
+          }
+        end
+      },
+    },
+  },
+
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
